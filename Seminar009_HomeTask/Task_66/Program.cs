d979fa5df64848c656803854c8cdb738011d4d2e
RomanGMonakhov <romangmonakhov@gmail.com>
2022-12-20 08:17:13 +0300

    add seminar009 hometask

---
+++ Seminar009_HomeTask/Task_66/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 66:
+Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+
+int SumMN(int a, int b)
+{
+    int res = b;
+    if (a != b) res = res + SumMN(a, b - 1);
+    return res;
+}
+
+Console.WriteLine("Enter M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum from M to N is: {SumMN(M, N)}");
---
+++ Seminar009_HomeTask/Task_68/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 68:
+Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.Clear();
+
+int Akk(int a, int b)
+{
+    int res = 0;
+    if (a == 0) res = b + 1;
+    else if (b == 0) res = Akk(a - 1, 1);
+    else if (a * b != 0) res = Akk(a - 1, Akk(a, b - 1));
+    return res; 
+}
+
+Console.WriteLine("Enter M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum from M to N is: {Akk(M, N)}");
