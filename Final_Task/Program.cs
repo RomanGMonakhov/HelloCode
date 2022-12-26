20b60353cf8584ce59489bc2d68d49fe03b5b836
RomanGMonakhov <romangmonakhov@gmail.com>
2022-12-26 08:22:40 +0300

    add Final_Task_Diagram

--- Final_Task/Program.cs
+++ Final_Task/Program.cs
@@ -6,5 +6,24 @@
 
 Console.Clear();
 
-string?[] array = new string?[];
+Console.WriteLine("Enter number of strings in array: ");
+int strings_count = int.Parse(Console.ReadLine());
 
+string[] array = new string[strings_count];
+
+void FillArray(ref string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Console.ReadLine();
+}
+
+void ShrinkArray(ref string[] array)
+{
+    array = array.Where(val => val.Length <= 3).ToArray();
+}
+
+FillArray(ref array);
+ShrinkArray(ref array);
+
+for (int i = 0; i < array.Length; i++)
+    Console.WriteLine(array[i]);
